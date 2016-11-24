e81d23d6c1f77d3980a433cd4c1b6930c6d491da
Luis Fernandez Alvarez <luis.fernandezalvarez@epfl.ch>
2016-11-24 15:11:10 +0100

    Add first version

---
+++ jupyterhub-oauth/jupyterhub_config.py
@@ -0,0 +1,45 @@
+import os
+import codecs
+
+c = get_config()
+
+
+
+##
+## Jupyterhub settings
+##
+
+c.JupyterHub.log_level = 10
+c.JupyterHub.cookie_secret_file = '/srv/jupyterhub/cookie_secret'
+c.JupyterHub.db_url = '/srv/jupyterhub/jupyterhub.sqlite'
+c.JupyterHub.ip = '0.0.0.0'
+c.JupyterHub.hub_ip = '0.0.0.0'
+c.JupyterHub.port = 8000
+c.JupyterHub.cleanup_servers = os.environ['CLEANUP_SERVERS'] == True
+
+
+
+##
+## Authentication configuration
+##
+
+c.JupyterHub.authenticator_class = 'oauthenticator.openshift.LocalOpenShiftOAuthenticator'
+c.LocalOpenShiftOAuthenticator.create_system_users = True
+# OpenShift Hack - 
+with codecs.open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r', encoding='utf-8') as secret:
+        c.OpenShiftOAuthenticator.client_secret = secret.read()
+
+
+##
+## Spawner configuration
+##
+
+c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
+# OpenShift Hack - The downward API doesn't provide yet information about the service name
+c.KubeSpawner.hub_connect_ip = os.environ["%s_SERVICE_HOST" % os.environ['SERVICE_NAME'].upper()]
+c.KubeSpawner.start_timeout = 60 * 5 
+c.KubeSpawner.singleuser_image_spec = 'yuvipanda/simple-singleuser:v1'
+c.KubeSpawner.mem_limit = '100M'
+c.KubeSpawner.mem_guarantee='100M'
+c.KubeSpawner.cpu_limit = 0.5
+c.KubeSpawner.cpu_guarantee = 0.5
