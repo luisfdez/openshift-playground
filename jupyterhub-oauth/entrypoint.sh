cf09f822c2c6d18cac287becdd025fcf468cbfb2
Luis Fernández Álvarez <luisfdez@users.noreply.github.com>
2017-01-16 15:09:07 +0100

    Add me :)

--- jupyterhub-oauth/entrypoint.sh
+++ jupyterhub-oauth/entrypoint.sh
@@ -4,6 +4,7 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 grep /etc/passwd > "$HOME/passwd"
 echo "jh_user:x:${USER_ID}:${GROUP_ID}:JupyterHub Server:${HOME}:/bin/bash" >> "$HOME/passwd"
+echo "lfernand:x:1:${GROUP_ID}:JupyterHub User:/home/lfernand:/bin/bash" >> "$HOME/passwd"
 export LD_PRELOAD=libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=${HOME}/passwd
 export NSS_WRAPPER_GROUP=/etc/group
