a306b20861f14e22b93d99b08326712816c33c02
Luis Fernandez Alvarez <luis.fernandezalvarez@epfl.ch>
2017-01-16 09:28:37 +0100

    Testing libnss logic

---
+++ jupyterhub-oauth/entrypoint.sh
@@ -0,0 +1,9 @@
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+grep /etc/passwd > "$HOME/passwd"
+echo "jh_user:x:${USER_ID}:${GROUP_ID}:JupyterHub Server:${HOME}:/bin/bash" >> "$HOME/passwd"
+export LD_PRELOAD=libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=${HOME}/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+
+jupyterhub
